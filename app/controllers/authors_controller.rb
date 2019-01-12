40e2a7c832ea460e188aceb343c918751f956028
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 23:04:07 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,9 @@ class AuthorsController < ApplicationController
   end
 
   def create
+
     @author = Author.new(author_params)
+
     if @author.save
       redirect_to author_path(@author)
     else
